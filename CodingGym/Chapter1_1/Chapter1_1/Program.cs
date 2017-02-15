97d2bf7c457ad5dcfc2e8e40e85edf583eee0a0a
bitsongit <shalabh@live.in>
2017-02-15 12:32:47 -0800

    https://leetcode.com/problems/decode-string/

--- CodingGym/Chapter1_1/Chapter1_1/Program.cs
+++ CodingGym/Chapter1_1/Chapter1_1/Program.cs
@@ -14,13 +14,69 @@ namespace Chapter1_1
                 Console.WriteLine("Enter String : ");
                 string str = Console.ReadLine();
 
-                Console.WriteLine("Given String Contains Unique Characters: {0}", IsUniqueChars(str));
+                Console.WriteLine("Given String Contains Unique Characters: {0}", DecodeString(str));
                 
                 Console.WriteLine("Earlier length : {0} Removed 'n' Characters: {1} ", str.Length,RemoveChar(str,'e'));
 
             }
             
         }
+        public static string DecodeString(string s)
+        {
+            if (string.IsNullOrEmpty(s))
+            {
+                return string.Empty;
+            }
+
+            Stack<int> count = new Stack<int>();
+            Stack<string> result = new Stack<string>();
+            int i = 0;
+            result.Push("");
+
+            while (i < s.Length)
+            {
+                char ch = s[i];
+
+                if (ch >= '0' && ch <= '9')
+                {
+                    int start = i;
+                    int nextNums = 0;
+                    while (s[i + 1] >= '0' && s[i + 1] <= '9')
+                    {
+                        i++;
+                        nextNums++;
+                    }
+                    string temp = s.Substring(start, nextNums + 1);
+
+                    Console.WriteLine(temp + ":" + start + ":" + (nextNums + 1));
+                    count.Push(int.Parse(temp));
+                }
+                else if (ch == '[')
+                {
+                    result.Push("");
+                }
+                else if (ch == ']')
+                {
+                    String str = result.Pop();
+                    StringBuilder sb = new StringBuilder();
+                    int times = count.Pop();
+                    for (int j = 0; j < times; j += 1)
+                    {
+                        sb.Append(str);
+                    }
+                    result.Push(result.Pop() + sb.ToString());
+                }
+                else
+                {
+                    result.Push(result.Pop() + ch);
+                }
+
+                i += 1;
+            }
+
+            return result.Pop();
+        }
+
 
         public static Boolean IsUniqueChars(String str)
         {
